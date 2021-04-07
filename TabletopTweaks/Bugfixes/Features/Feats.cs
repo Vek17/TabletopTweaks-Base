06ba1fe60aca0f710112522decd609b3d77ef8fd
Sean Petrie <sean.petrie17@gmail.com>
2021-04-06 21:29:44 -0500

    Added CraneWing patch

---
+++ TabletopTweaks/Bugfixes/Features/Feats.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using System.Linq;
+using Kingmaker.Designers.EventConditionActionSystem.Actions;
+using TabletopTweaks.Utilities;
+using TabletopTweaks.NewComponents;
+
+namespace TabletopTweaks.Bugfixes.Features {
+    class Feats {
+        [HarmonyPatch(typeof(ResourcesLibrary), "InitializeLibrary")]
+        static class ResourcesLibrary_InitializeLibrary_Patch {
+            static bool Initialized;
+            static bool Prefix() {
+                if (Initialized) {
+                    // When wrath first loads into the main menu InitializeLibrary is called by Kingmaker.GameStarter.
+                    // When loading into maps, Kingmaker.Runner.Start will call InitializeLibrary which will
+                    // clear the ResourcesLibrary.s_LoadedBlueprints cache which causes loaded blueprints to be garbage collected.
+                    // Return false here to prevent ResourcesLibrary.InitializeLibrary from being called twice 
+                    // to prevent blueprints from being garbage collected.
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+            static void Postfix() {
+                if (Initialized) return;
+                Initialized = true;
+                if (Resources.Fixes.Feats.DisableAllFixes) { return; }
+                Main.LogHeader("Patching Feat Resources");
+                patchCraneWing();
+                Main.LogHeader("Patching Feat Resources Complete");
+                //Do Stuff
+            }
+            static void patchCraneWing() {
+                if (!Resources.Fixes.Feats.Fixes["CraneWing"]) { return; }
+                BlueprintBuff CraneStyleBuff = ResourcesLibrary.TryGetBlueprint<BlueprintBuff>("e8ea7bd10136195478d8a5fc5a44c7da");
+                var FightingDefensivlyTrigger = CraneStyleBuff.GetComponent<AddInitiatorAttackWithWeaponTrigger>();
+                var Conditionals = FightingDefensivlyTrigger.Action.Actions.OfType<Conditional>();
+
+                var newConditonal = Helpers.Create<ContextConditionHasFreeHand>();
+                Conditionals.First().ConditionsChecker.Conditions = Conditionals.First().ConditionsChecker.Conditions.AddItem(newConditonal).ToArray();
+
+                Main.LogPatch("Patched", CraneStyleBuff);
+            }
+        }
+    }
+}
---
+++ TabletopTweaks/NewComponents/ContextConditionHasFreeHand.cs
@@ -0,0 +1,27 @@
+﻿using Kingmaker.Blueprints.Items.Armors;
+using Kingmaker.UnitLogic.Mechanics.Conditions;
+
+namespace TabletopTweaks.NewComponents {
+	public class ContextConditionHasFreeHand : ContextCondition {
+		public override string GetConditionCaption() {
+			return "Check if caster has a free hand";
+		}
+
+        public override bool CheckCondition() {
+			var secondaryHand = Target.Unit.Body.CurrentHandsEquipmentSet.SecondaryHand;
+			var primaryHand = Target.Unit.Body.CurrentHandsEquipmentSet.PrimaryHand;
+			bool hasFreeHand = true;
+            if (secondaryHand.HasShield) {
+				var maybeShield = secondaryHand.MaybeShield;
+				hasFreeHand = maybeShield.Blueprint.Type.ProficiencyGroup == ArmorProficiencyGroup.Buckler ? true : false;
+			}
+            if (!secondaryHand.HasWeapon || secondaryHand.MaybeWeapon == Target.Unit.Body.EmptyHandWeapon) {
+                if (primaryHand.HasWeapon) {
+					hasFreeHand = primaryHand.MaybeWeapon.HoldInTwoHands ? false : hasFreeHand;
+				}
+			}
+			//Main.Log($"Check Result: {hasFreeHand}");
+			return hasFreeHand;
+		}
+	}
+}
