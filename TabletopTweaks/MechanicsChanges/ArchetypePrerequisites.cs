2f96924bce98957904a6f898474e628a5a07c87f
Sean Petrie <sean.petrie17@gmail.com>
2021-04-09 03:36:15 -0500

    Cleaned up logic

--- TabletopTweaks/MechanicsChanges/ArchetypePrerequisites.cs
+++ TabletopTweaks/MechanicsChanges/ArchetypePrerequisites.cs
@@ -12,7 +12,7 @@ using System.Linq;
 
 namespace TabletopTweaks.MechanicsChanges {
 
-    // UI Hack to remove unqualified Archetypes from the list
+    // Forces Archetypes to Consider Prerequisites
     [HarmonyPatch(typeof(CharGenClassSelectorItemVM), "GetArchetypesList", typeof(BlueprintCharacterClass))]
     static class CharGenClassPhaseVM_GetArchetypeList_patch {
         static void Postfix(ref List<NestedSelectionGroupEntityVM>  __result, CharGenClassSelectorItemVM __instance, BlueprintCharacterClass selectedClass) {
@@ -21,10 +21,6 @@ namespace TabletopTweaks.MechanicsChanges {
                 __result =  list;
                 return;
             }
-            /*
-            list.AddRange((from archetype in selectedClass.Archetypes
-                           select new CharGenClassSelectorItemVM(selectedClass, archetype, __instance.LevelUpController, __instance, __instance.SelectedArchetype, __instance.m_InfoVM, MeetsPrerequisites(archetype), true)).ToList<CharGenClassSelectorItemVM>()) ;
-            */
 
             list.AddRange(selectedClass.Archetypes
                 .Select(archetype => new CharGenClassSelectorItemVM(
@@ -42,39 +38,32 @@ namespace TabletopTweaks.MechanicsChanges {
             bool MeetsPrerequisites(BlueprintArchetype archetype) {
                 UnitDescriptor unit = __instance.LevelUpController.Unit;
                 LevelUpState state = __instance.LevelUpController.State;
-                if (IgnorePrerequisites.Ignore) {
-                    return true;
-                }
                 int classLevel = unit.Progression.GetClassLevel(archetype.GetParentClass());
-                if (classLevel >= 20) {
-                    return false;
-                }
-                bool? flag = null;
-                bool? flag2 = null;
-                for (int i = 0; i < archetype.ComponentsArray.Length; i++) {
-                    Prerequisite prerequisite = archetype.ComponentsArray[i] as Prerequisite;
-                    if (prerequisite) {
-                        bool flag3 = prerequisite.Check(null, unit, state);
-                        switch (prerequisite.Group) {
-                            case Prerequisite.GroupType.All:
-                                flag = new bool?((flag == null) ? flag3 : (flag.Value && flag3));
-                                break;
-                            case Prerequisite.GroupType.Any:
-                                flag2 = new bool?((flag2 == null) ? flag3 : (flag2.Value || flag3));
-                                break;
-                            case Prerequisite.GroupType.ForcedTrue:
-                                if (flag3) {
-                                    return true;
-                                }
-                                break;
-                        }
+                if (IgnorePrerequisites.Ignore) { return true; }
+                if (classLevel >= 20) { return false; }
+
+                bool? All = null;
+                bool? Any = null;
+                foreach (Prerequisite prerequisite in archetype.GetComponents<Prerequisite>()) {
+                    bool Check = prerequisite.Check(null, unit, state);
+                    switch (prerequisite.Group) {
+                        case Prerequisite.GroupType.All:
+                            All = (All == null) ? Check : All.Value && Check;
+                            break;
+                        case Prerequisite.GroupType.Any:
+                            Any = (Any == null) ? Check : Any.Value || Check;
+                            break;
+                        case Prerequisite.GroupType.ForcedTrue:
+                            if (Check) { return true; }
+                            break;
                     }
                 }
-                return (flag ?? true) && (flag2 ?? true);
+                return (All ?? true) & (Any ?? true);
             }
         }
     }
 
+    // Forces Archetypes to Display Prerequisites
     [HarmonyPatch(typeof(TooltipTemplateLevelUp), "AddClassPrerequisites")]
     static class CharGenClassPhaseVM_AddClassPrerequisites_patch {
         static void Postfix(TooltipTemplateLevelUp __instance, List<ITooltipBrick> bricks) {
