d2f670626ce966aceff6f0675fd6c18abc4e57ac
Sean Petrie <sean.petrie17@gmail.com>
2021-04-21 20:34:06 -0500

    Added fix for improper nested selection handling of prerequisites

---
+++ TabletopTweaks/Bugfixes/UI/FeatureSelection.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Selection;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Class.LevelUp;
+using System.Linq;
+
+namespace TabletopTweaks.Bugfixes.UI {
+    class FeatureSelection {
+        [HarmonyPatch(typeof(NestedFeatureSelectionUtils), "AllNestedFeaturesUnavailable", new[] { typeof(UnitDescriptor), typeof(LevelUpState), typeof(FeatureSelectionState), typeof(BlueprintFeature) })]
+        static class NestedFeatureSelectionUtils_AllNestedFeaturesUnavailable_Patch {
+
+            static bool Prefix(ref bool __result, UnitDescriptor unit, LevelUpState state, FeatureSelectionState selectionState, BlueprintFeature feature) {
+				IFeatureSelection selection = (feature as IFeatureSelection);
+				if (selection == null) { return; }
+				FeatureSelectionState newSelectionState = new FeatureSelectionState(selectionState, selectionState.Source, selection, 0, 0);
+				Main.LogDebug($"NEST: {feature.name} - IgnorePrerequisites: {newSelectionState.IgnorePrerequisites}");
+				__result = !selection.CanSelectAny(unit, state, newSelectionState);
+				Main.LogDebug($"NEST RESULT: {selection} - {__result}");
+				return false;
+			}
+		}
+		[HarmonyPatch(typeof(FeatureSelectionExtensions), "CanSelectAny", new[] { typeof(IFeatureSelection), typeof(UnitDescriptor), typeof(LevelUpState), typeof(FeatureSelectionState) })]
+		static class FeatureSelectionExtensions_CanSelectAny_Patch {
+
+			static bool Prefix(ref bool __result, IFeatureSelection selection, UnitDescriptor unit, LevelUpState state, FeatureSelectionState selectionState) {
+				FeatureSelectionState newSelectionState = new FeatureSelectionState(selectionState, selectionState.Source, selection, 0, 0);
+				Main.LogDebug($"ANY: {selection} - IgnorePrerequisites: {newSelectionState.IgnorePrerequisites}");
+				__result = selection.Items.Any((IFeatureSelectionItem item) => selection.CanSelect(unit, state, newSelectionState, item));
+				Main.LogDebug($"ANY RESULT: {selection} - {__result}");
+				return false;
+			}
+		}
+	}
+}
