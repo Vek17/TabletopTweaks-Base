918ab137d140fac236f57a612b2d9af3a8fd34ce
Sean Petrie <sean.petrie17@gmail.com>
2021-04-26 21:54:33 -0500

    Added SpellUtils

---
+++ TabletopTweaks/Utilities/SpellTools.cs
@@ -0,0 +1,159 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using System;
+using System.Linq;
+using TabletopTweaks.Extensions;
+
+namespace TabletopTweaks.Utilities {
+    static class SpellTools {
+        public static void AddToSpellList(this BlueprintAbility spell, BlueprintSpellList spellList, int level) {
+            var comp = Helpers.Create<SpellListComponent>();
+            comp.SpellLevel = level;
+            comp.m_SpellList = spellList.ToReference<BlueprintSpellListReference>();
+            spell.AddComponent(comp);
+            spellList.SpellsByLevel[level].Spells.Add(spell);
+            if (spellList == SpellList.WizardSpellList) {
+                var school = spell.School;
+                var specialistList = specialistSchoolList.Value[(int)school];
+                specialistList?.SpellsByLevel[level].Spells.Add(spell);
+
+                for (int i = 0; i < thassilonianSchoolList.Value.Length; i++) {
+                    if (thassilonianOpposedSchools.Value[i] != null && !thassilonianOpposedSchools.Value[i].Contains(school)) {
+                        thassilonianSchoolList.Value[i]?.SpellsByLevel[level].Spells.Add(spell);
+                    }
+                }
+
+                if (school == SpellSchool.Enchantment || school == SpellSchool.Illusion) {
+                    SpellList.FeyspeakerSpelllist.SpellsByLevel[level].Spells.Add(spell);
+                }
+            }
+        }
+        static readonly Lazy<BlueprintSpellList[]> specialistSchoolList = new Lazy<BlueprintSpellList[]>(() => {
+            var result = new BlueprintSpellList[(int)SpellSchool.Universalist + 1];
+            result[(int)SpellSchool.Abjuration] = SpellList.WizardAbjurationSpellList;
+            result[(int)SpellSchool.Conjuration] = SpellList.WizardConjurationSpellList;
+            result[(int)SpellSchool.Divination] = SpellList.WizardDivinationSpellList;
+            result[(int)SpellSchool.Enchantment] = SpellList.WizardEnchantmentSpellList;
+            result[(int)SpellSchool.Evocation] = SpellList.WizardEvocationSpellList;
+            result[(int)SpellSchool.Illusion] = SpellList.WizardIllusionSpellList;
+            result[(int)SpellSchool.Necromancy] = SpellList.WizardNecromancySpellList;
+            result[(int)SpellSchool.Transmutation] = SpellList.WizardTransmutationSpellList;
+            return result;
+        });
+        static readonly Lazy<BlueprintSpellList[]> thassilonianSchoolList = new Lazy<BlueprintSpellList[]>(() => {
+            var result = new BlueprintSpellList[(int)SpellSchool.Universalist + 1];
+            result[(int)SpellSchool.Abjuration] = SpellList.ThassilonianAbjurationSpellList;
+            result[(int)SpellSchool.Conjuration] = SpellList.ThassilonianConjurationSpellList;
+            result[(int)SpellSchool.Enchantment] = SpellList.ThassilonianEnchantmentSpellList;
+            result[(int)SpellSchool.Evocation] = SpellList.ThassilonianEvocationSpellList;
+            result[(int)SpellSchool.Illusion] = SpellList.ThassilonianIllusionSpellList;
+            result[(int)SpellSchool.Necromancy] = SpellList.ThassilonianNecromancySpellList;
+            result[(int)SpellSchool.Transmutation] = SpellList.ThassilonianTransmutationSpellList;
+            return result;
+        });
+        static readonly Lazy<SpellSchool[][]> thassilonianOpposedSchools = new Lazy<SpellSchool[][]>(() => {
+            var result = new SpellSchool[(int)SpellSchool.Universalist + 1][];
+
+            result[(int)SpellSchool.Abjuration] = new SpellSchool[] { SpellSchool.Evocation, SpellSchool.Necromancy };
+            result[(int)SpellSchool.Conjuration] = new SpellSchool[] { SpellSchool.Evocation, SpellSchool.Illusion };
+            result[(int)SpellSchool.Enchantment] = new SpellSchool[] { SpellSchool.Necromancy, SpellSchool.Transmutation };
+            result[(int)SpellSchool.Evocation] = new SpellSchool[] { SpellSchool.Abjuration, SpellSchool.Conjuration };
+            result[(int)SpellSchool.Illusion] = new SpellSchool[] { SpellSchool.Conjuration, SpellSchool.Transmutation };
+            result[(int)SpellSchool.Necromancy] = new SpellSchool[] { SpellSchool.Abjuration, SpellSchool.Enchantment };
+            result[(int)SpellSchool.Transmutation] = new SpellSchool[] { SpellSchool.Enchantment, SpellSchool.Illusion };
+            return result;
+        });
+        public static class SpellList {
+            public static readonly BlueprintSpellList AeonSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("24b0c796f723a144e9891b6c4794c595");
+            public static readonly BlueprintSpellList AeonSpellMythicList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("ca8c6024bd2519f4b97162a3ad286920");
+            public static readonly BlueprintSpellList AirDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("9678d121f669f864d9da5dabf1ca1ce0");
+            public static readonly BlueprintSpellList AlchemistSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("f60d0cd93edc65c42ad31e34a905fb2f");
+            public static readonly BlueprintSpellList AngelClericSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("c074062863fbc1e4bab02f9e6e4eb78d");
+            public static readonly BlueprintSpellList AngelMythicSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("deaffb4218ccf2f419ffd6e41603131a");
+            public static readonly BlueprintSpellList AnimalDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("d0ccda70fddc0e346a227068502771c1");
+            public static readonly BlueprintSpellList ArmagsBladeSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("3ea72a95cce88b4449a917ad0a0f36da");
+            public static readonly BlueprintSpellList ArtificeDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("5ef652f325d21414d8565fcfb19d8177");
+            public static readonly BlueprintSpellList AzataMythicSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("db216faa0ff2b984399e7495755c7111");
+            public static readonly BlueprintSpellList AzataMythicSpellsSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("10c634d2b386d8d41b18a889adb8cd49");
+            public static readonly BlueprintSpellList BardSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("25a5013493bdcf74bb2424532214d0c8");
+            public static readonly BlueprintSpellList BattleSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("e788846aaf449404db11e51182174be8");
+            public static readonly BlueprintSpellList BloodragerSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("98c05aeff6e3d384f8aec6d584973642");
+            public static readonly BlueprintSpellList BonesSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("24dc5eb552a9e2c4cb9a17c355a80d2e");
+            public static readonly BlueprintSpellList ChaosDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("2ff9831eb262758449287f820108428d");
+            public static readonly BlueprintSpellList CharmDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("31c742d02fc33204cad4e02dddf028dd");
+            public static readonly BlueprintSpellList ClericSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("8443ce803d2d31347897a3d85cc32f53");
+            public static readonly BlueprintSpellList CommunityDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("75576ed8cab010644a11f9ecd512a7f9");
+            public static readonly BlueprintSpellList DarknessDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("fa79e8d1fe20b0e43bf3ebca4cef93b9");
+            public static readonly BlueprintSpellList DeathDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("436986e90d1e81b45a1accb6fa7261f0");
+            public static readonly BlueprintSpellList DemonSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("abb1991bf6e996348bb743471ee7e1c1");
+            public static readonly BlueprintSpellList DemonUsualSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("78721d556676f264da947bfe263b1da0");
+            public static readonly BlueprintSpellList DestructionDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("6f9fe425726026d4f9c28c32d5d03481");
+            public static readonly BlueprintSpellList DruidSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("bad8638d40639d04fa2f80a1cac67d6b");
+            public static readonly BlueprintSpellList EarthDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("df3bc5bda7deb9d46b0f177db3bb7876");
+            public static readonly BlueprintSpellList EvilDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("81bff1165d9468a44b2f815f7c26a373");
+            public static readonly BlueprintSpellList FeyspeakerSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("640b4c89527334e45b19d884dd82e500");
+            public static readonly BlueprintSpellList FireDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("d8f30625d1b1f9d41a24446cbf7ac52e");
+            public static readonly BlueprintSpellList FlamesSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("659fbc54fc519b44dacacc78e7d46dec");
+            public static readonly BlueprintSpellList FrostSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("bbae401660bbad94c865d71029d8439e");
+            public static readonly BlueprintSpellList GloryDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("7b3506924ed8354419b7829736ab2c7e");
+            public static readonly BlueprintSpellList GoodDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("dc242eb60eed94a4eb0640d773780090");
+            public static readonly BlueprintSpellList HealingDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("033b2b6a8899be844ae8aa91d4dab477");
+            public static readonly BlueprintSpellList HunterSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("d090b791bfe381740b98ed4ff909b1cf");
+            public static readonly BlueprintSpellList InquisitorSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("57c894665b7895c499b3dce058c284b3");
+            public static readonly BlueprintSpellList KnowledgeDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("384627980c2a60a43800f14029fbb8a7");
+            public static readonly BlueprintSpellList LawDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("57b0bbdc1114ee846945f1808b13cff7");
+            public static readonly BlueprintSpellList LiberationDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("73406d3b9f6290e499c5fa3951a24234");
+            public static readonly BlueprintSpellList LichMythicSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("a06bcb035c214ad4db605491be9da13d");
+            public static readonly BlueprintSpellList LichSkeletalIBardMinorSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("0fbd00235f7042841ba1a3fcae0dbc58");
+            public static readonly BlueprintSpellList LichSkeletalInquisitorMinorSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("c7970aa2f3c94e245942369d348c0a1f");
+            public static readonly BlueprintSpellList LichWizardSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("7d5987082120bb943ac96cde7b2257ad");
+            public static readonly BlueprintSpellList LifeSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("1e6ea0d1d642c8c43ab7e72dd8f607a9");
+            public static readonly BlueprintSpellList LuckDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("9e756552e9b05ce459feac658dd2d8fb");
+            public static readonly BlueprintSpellList MadnessDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("1d1638d47e7f8404baeed23bc35ec2f2");
+            public static readonly BlueprintSpellList MagicDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("f997258a28a9e5f4192d973433edec5f");
+            public static readonly BlueprintSpellList MagusSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("4d72e1e7bd6bc4f4caaea7aa43a14639");
+            public static readonly BlueprintSpellList MonsterEmptySpellllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("9e4658592e5f66146a6826120e21ed26");
+            public static readonly BlueprintSpellList NatureSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("810e2a7009ed3a84c81b398b2763e7a8");
+            public static readonly BlueprintSpellList NobilityDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("3de1e283971828f4896a4140acd3c84c");
+            public static readonly BlueprintSpellList PaladinSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("9f5be2f7ea64fe04eb40878347b147bc");
+            public static readonly BlueprintSpellList PlantDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("bd7b088a54b79434f90ed53551ca2189");
+            public static readonly BlueprintSpellList ProtectionDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("93228f4df23d2d448a0db59141af8aed");
+            public static readonly BlueprintSpellList RangerSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("29f3c338532390546bc5347826a655c4");
+            public static readonly BlueprintSpellList ReposeDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("5376474a39713514ca2135d6f9584563");
+            public static readonly BlueprintSpellList RuneDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("30076fe3d5f4ef845a7bafb0be57fe44");
+            public static readonly BlueprintSpellList ShamanSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("c0c40e42f07ff104fa85492da464ac69");
+            public static readonly BlueprintSpellList SpiritWardenSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("767db8f0d64e5b048a8cf4d2ddc10521");
+            public static readonly BlueprintSpellList StoneSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("87a3e296757412e45910493e5fed1417");
+            public static readonly BlueprintSpellList StrengthDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("03db76fd27428004482f9314c334d1ab");
+            public static readonly BlueprintSpellList SunDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("600ffed45d0c3ec43a75dc76bb9377b6");
+            public static readonly BlueprintSpellList ThassilonianAbjurationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("280dd5167ccafe449a33fbe93c7a875e");
+            public static readonly BlueprintSpellList ThassilonianConjurationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("5b154578f228c174bac546b6c29886ce");
+            public static readonly BlueprintSpellList ThassilonianEnchantmentSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("ac551db78c1baa34eb8edca088be13cb");
+            public static readonly BlueprintSpellList ThassilonianEvocationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("17c0bfe5b7c8ac3449da655cdcaed4e7");
+            public static readonly BlueprintSpellList ThassilonianIllusionSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("c311aed33deb7a346ab715baef4a0572");
+            public static readonly BlueprintSpellList ThassilonianNecromancySpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("5c08349132cb6b04181797f58ccf38ae");
+            public static readonly BlueprintSpellList ThassilonianTransmutationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("f3a8f76b1d030a64084355ba3eea369a");
+            public static readonly BlueprintSpellList TravelDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("ab90308db82342f47bf0d636fe941434");
+            public static readonly BlueprintSpellList TrickeryDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("2c09ae283ea3e41408665c46fcf0303f");
+            public static readonly BlueprintSpellList TricksterSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("40f8cfe98ca4ebd43894267dbd3fc3ae");
+            public static readonly BlueprintSpellList TricksterSpelllistMythic = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("7a5ea54564c7d494794f34d0f5a9abb3");
+            public static readonly BlueprintSpellList WarDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("e3c54da90a2b54b4a975a80b5d39c361");
+            public static readonly BlueprintSpellList WarpriestSpelllist = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("c5a1b8df32914d74c9b44052ba3e686a");
+            public static readonly BlueprintSpellList WaterDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("35e15cd1b353e2d47b507c445d2f8c6f");
+            public static readonly BlueprintSpellList WavesSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("190ebde2d9d80c94783bcb73b9106d99");
+            public static readonly BlueprintSpellList WeatherDomainSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("eba577470b8ee8443bb4552433451990");
+            public static readonly BlueprintSpellList WindSpiritSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("0bf6f90fdcb864b4486344100391b478");
+            public static readonly BlueprintSpellList WitchSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("e17df9977b879b64e8a8cbb4b3569f19");
+            public static readonly BlueprintSpellList WizardAbjurationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("c7a55e475659a944f9229d89c4dc3a8e");
+            public static readonly BlueprintSpellList WizardConjurationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("69a6eba12bc77ea4191f573d63c9df12");
+            public static readonly BlueprintSpellList WizardDivinationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("d234e68b3d34d124a9a2550fdc3de9eb");
+            public static readonly BlueprintSpellList WizardEnchantmentSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("c72836bb669f0c04680c01d88d49bb0c");
+            public static readonly BlueprintSpellList WizardEvocationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("79e731172a2dc1f4d92ba229c6216502");
+            public static readonly BlueprintSpellList WizardIllusionSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("d74e55204daa9b14993b2e51ae861501");
+            public static readonly BlueprintSpellList WizardNecromancySpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("5fe3acb6f439db9438db7d396f02c75c");
+            public static readonly BlueprintSpellList WizardSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("ba0401fdeb4062f40a7aa95b6f07fe89");
+            public static readonly BlueprintSpellList WizardTransmutationSpellList = ResourcesLibrary.TryGetBlueprint<BlueprintSpellList>("becbcfeca9624b6469319209c2a6b7f1");
+        }
+    }
+}
