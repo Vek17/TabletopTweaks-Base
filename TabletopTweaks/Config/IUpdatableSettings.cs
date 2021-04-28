e8506f07e1e6535e6df4fa1019a5be7f13154dca
Sean Petrie <sean.petrie17@gmail.com>
2021-04-28 17:51:47 -0500

    Updated Settings to be more generic

---
+++ TabletopTweaks/Config/IUpdatableSettings.cs
@@ -0,0 +1,5 @@
+﻿namespace TabletopTweaks.Config {
+    public interface IUpdatableSettings {
+        void OverrideSettings(IUpdatableSettings userSettings);
+    }
+}
---
+++ TabletopTweaks/Config/ModSettings.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+using static UnityModManagerNet.UnityModManager;
+
+namespace TabletopTweaks.Config {
+    static class ModSettings {
+        public static ModEntry ModEntry;
+        private static Fixes fixes;
+        public static Fixes Fixes {
+            get {
+                if (fixes == null) {
+                    LoadAllSettings();
+                }
+                return fixes;
+            }
+        }
+        private static AddedContent addedContent;
+        public static AddedContent AddedContent {
+            get {
+                if (addedContent == null) {
+                    LoadAllSettings();
+                }
+                return addedContent;
+            }
+        }
+        private static Blueprints blueprints;
+        public static Blueprints Blueprints {
+            get {
+                if (addedContent == null) {
+                    LoadAllSettings();
+                }
+                return blueprints;
+            }
+        }
+
+        public static void LoadAllSettings() {
+            LoadSettings("Fixes.json", ref fixes);
+            LoadSettings("AddedContent.json", ref addedContent);
+            LoadSettings("Blueprints.json", ref blueprints);
+        }
+        private static void LoadSettings<T>(string fileName, ref T setting) where T : IUpdatableSettings {
+            var assembly = Assembly.GetExecutingAssembly();
+            string userConfigFolder = ModEntry.Path + "UserSettings";
+            Directory.CreateDirectory(userConfigFolder);
+            var resourcePath = $"TabletopTweaks.Config.{fileName}";
+            var userPath = $"{userConfigFolder}{Path.DirectorySeparatorChar}{fileName}";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourcePath))
+            using (StreamReader reader = new StreamReader(stream)) {
+                setting = JsonConvert.DeserializeObject<T>(reader.ReadToEnd());
+            }
+            if (File.Exists(userPath)) {
+                using (StreamReader reader = File.OpenText(userPath)) {
+                    try {
+                        T userSettings = JsonConvert.DeserializeObject<T>(reader.ReadToEnd());
+                        setting.OverrideSettings(userSettings);
+                    } catch {
+                        Main.Error("Failed to load user settings. Settings will be rebuilt.");
+                        try { File.Copy(userPath, userConfigFolder + $"{Path.DirectorySeparatorChar}BROKEN_{fileName}", true); } catch { Main.Error("Failed to archive broken settings."); }
+                    }
+                }
+            }
+            File.WriteAllText(userPath, JsonConvert.SerializeObject(setting, Formatting.Indented));
+        }
+    }
+}
