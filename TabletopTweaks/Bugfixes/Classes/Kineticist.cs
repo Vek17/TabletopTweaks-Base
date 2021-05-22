81539e8f0a80b54b5b4fe7e7a117f69a557b1916
Sean Petrie <sean.petrie17@gmail.com>
2021-05-21 22:54:03 -0500

    Removed elemental engine fix

--- TabletopTweaks/Bugfixes/Classes/Kineticist.cs
+++ TabletopTweaks/Bugfixes/Classes/Kineticist.cs
@@ -15,24 +15,11 @@ namespace TabletopTweaks.Bugfixes.Classes {
                 if (ModSettings.Fixes.Kineticist.DisableAll) { return; }
                 Main.LogHeader("Patching Kineticist");
                 PatchBase();
-                PatchElementalEngine();
             }
             static void PatchBase() {
                 if (ModSettings.Fixes.Kineticist.Base.DisableAll) { return; }
                 var ElementalOverflowProgression = Resources.GetBlueprint<BlueprintFeatureBase>("86beb0391653faf43aec60d5ec05b538");
                 ElementalOverflowProgression.HideInUI = false;
-            }
-            static void PatchElementalEngine() {
-                if (ModSettings.Fixes.Kineticist.Archetypes["ElementalEngine"].DisableAll) { return; }
-                if (!ModSettings.Fixes.Kineticist.Archetypes["ElementalEngine"].Enabled["FixBrokenSelection"]) { return; }
-                var ElementalEngine = Resources.GetBlueprint<BlueprintArchetype>("4a8324d676d642c99edcdda6988ca3b1");
-                var Supercharge = Resources.GetBlueprint<BlueprintFeatureBase>("5a13756fb4be25f46951bc3f16448276");
-                var ElementalOverflowProgression = Resources.GetBlueprint<BlueprintFeatureBase>("86beb0391653faf43aec60d5ec05b538");
-                ElementalEngine.RemoveFeatures = new LevelEntry[] {
-                    new LevelEntry(){ Level = 1, Features = { ElementalOverflowProgression }},
-                    new LevelEntry(){ Level = 11, Features = { Supercharge }}
-                };
-                Main.LogPatch("Patched", ElementalEngine);
             }
         }
     }
