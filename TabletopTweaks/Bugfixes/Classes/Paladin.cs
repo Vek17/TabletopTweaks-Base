03ec913d4a405e5cd9b5e6dc55af24dac0bf92e7
Sean Petrie <sean.petrie17@gmail.com>
2021-08-06 10:53:19 -0500

    Removed Divine Mount Fix

--- TabletopTweaks/Bugfixes/Classes/Paladin.cs
+++ TabletopTweaks/Bugfixes/Classes/Paladin.cs
@@ -16,17 +16,8 @@ namespace TabletopTweaks.Bugfixes.Classes {
                 Initialized = true;
                 if (ModSettings.Fixes.Paladin.DisableAll) { return; }
                 Main.LogHeader("Patching Paladin");
-                PatchDivineMountSelection();
             }
-            static void PatchDivineMountSelection() {
-                if (!ModSettings.Fixes.Paladin.Base.Enabled["DivineMountSelection"]) { return; }
-                var PaladinDivineMountSelection = Resources.GetBlueprint<BlueprintFeatureSelection>("e2f0e0efc9e155e43ba431984429678e");
-                var AnimalCompanionEmptyCompanion = Resources.GetBlueprint<BlueprintFeature>("472091361cf118049a2b4339c4ea836a");
-                var AnimalCompanionFeatureHorse = Resources.GetBlueprint<BlueprintFeature>("9dc58b5901677c942854019d1dd98374");
-
-                PaladinDivineMountSelection.SetFeatures(AnimalCompanionEmptyCompanion, AnimalCompanionFeatureHorse);
-                PaladinDivineMountSelection.m_Features = PaladinDivineMountSelection.m_AllFeatures;
-                Main.LogPatch("Patched", PaladinDivineMountSelection);
+            static void PatchBase() {
             }
             static void PatchArchetypes() {
             }
