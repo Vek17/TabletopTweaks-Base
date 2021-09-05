8bfe3847ed19a887c5d9e1da73ea2e95cb523af1
Sean Petrie <sean.petrie17@gmail.com>
2021-09-05 09:29:04 -0500

    Fixed issue with mistagged dragonkind breath weapons

--- TabletopTweaks/MechanicsChanges/PolymorphStacking.cs
+++ TabletopTweaks/MechanicsChanges/PolymorphStacking.cs
@@ -45,10 +45,11 @@ namespace TabletopTweaks.MechanicsChanges {
                 Initialized = true;
                 if (!ModSettings.Fixes.DisablePolymorphStacking) { return; }
                 Main.LogHeader("Patching Polymorph Effects");
-                FixModifers();
+                AddModifers();
+                RemoveModifiers();
 
             }
-            static void FixModifers() {
+            static void AddModifers() {
                 IEnumerable<BlueprintBuff> polymorphBuffs = new List<BlueprintBuff>() {
                     Resources.GetBlueprint<BlueprintBuff>("082caf8c1005f114ba6375a867f638cf"), //GeniekindDjinniBuff  
                     Resources.GetBlueprint<BlueprintBuff>("d47f45f29c4cfc0469f3734d02545e0b"), //GeniekindEfreetiBuff  
@@ -66,6 +67,30 @@ namespace TabletopTweaks.MechanicsChanges {
                                 c.Descriptor = SpellDescriptor.Polymorph;
                             }));
                         }
+                        Main.LogPatch("Patched", buff);
+                    });
+            }
+            static void RemoveModifiers() {
+                IEnumerable<BlueprintBuff> polymorphBuffs = new List<BlueprintBuff>() {
+                    Resources.GetBlueprint<BlueprintBuff>("ae5d58afe8b9aa14cae977d36ff090c8"), //FormOfTheDragonISilverBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("43868c29760f7204f996dcc99ec93b39"), //FormOfTheDragonIISilverBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("b78d21189e7f6e943920236f009d30e3"), //FormOfTheDragonIIIBreathWeaponCooldownBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("5effa97644bb7394d8532c216ec0f216"), //FormOfTheDragonIIGreenBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("81e3330720af3d04eb65d9a2e7d92abb"), //FormOfTheDragonIIGoldBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("a7a5d1143490dae49b8603810866cf4d"), //FormOfTheDragonIIBreathWeaponCooldownBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("a81c1b775d3da144ea8d3c43c5b349a2"), //FormOfTheDragonIIBrassBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("ae5c9458a570b334d8dac0774f62efa1"), //FormOfTheDragonIIBlueBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("7c21715c4c4938b40ac2996a1330eeb2"), //FormOfTheDragonIIBlackBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("3980fce6bbc14604e99fcd77b326220e"), //FormOfTheDragonIGreenBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("3eebb97861a3405418396e1b9866be72"), //FormOfTheDragonIGoldBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("e8307c93669e05c4ea235a70bf5c8f98"), //FormOfTheDragonIBrassBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("93e27994169df9c43885394dc68f137f"), //FormOfTheDragonIBlueBreathWeaponBuff  
+                    Resources.GetBlueprint<BlueprintBuff>("5d7089b61f459204993a1292d6f158f8")  //FormOfTheDragonIBlackBreathWeaponBuff  
+                };
+                polymorphBuffs
+                    .OrderBy(buff => buff.name)
+                    .ForEach(buff => {
+                        buff.RemoveComponents<SpellDescriptorComponent>(c => c.Descriptor.HasAnyFlag(SpellDescriptor.Polymorph));
                         Main.LogPatch("Patched", buff);
                     });
             }
