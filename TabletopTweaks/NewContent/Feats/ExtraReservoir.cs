de719aced1baf8f0d65c80f30bc3eceffb08b029
Sean Petrie <sean.petrie17@gmail.com>
2021-09-15 19:23:00 -0700

    updated warning suppression

--- TabletopTweaks/NewContent/Feats/ExtraReservoir.cs
+++ TabletopTweaks/NewContent/Feats/ExtraReservoir.cs
@@ -16,7 +16,6 @@ namespace TabletopTweaks.NewContent.Feats {
             var ArcanistArcaneReservoirFeature = Resources.GetBlueprint<BlueprintFeature>("55db1859bd72fd04f9bd3fe1f10e4cbb");
             var ArcaneEnforcerArcaneReservoirFeature = Resources.GetBlueprint<BlueprintFeature>("9d1e2212594cf47438fff2fa3477b954");
 
-
             var ExtraReservoir = FeatTools.CreateExtraResourceFeat("ExtraReservoir", ArcanistArcaneReservoirResource, 3, bp => {
                 bp.SetName("Extra Reservoir");
                 bp.SetDescription("You gain three more points in your arcane reservoir, and the maximum number of points in your arcane reservoir " +
