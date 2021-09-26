9fe418516e47a280e82d67d9290176f7f447ca2d
Aegonek <53911798+Aegonek@users.noreply.github.com>
2021-09-26 22:55:33 +0200

    Delete AfterBuild.ps1

--- TabletopTweaks/Scripts/AfterBuild.ps1
+++
@@ -1,5 +0,0 @@
-param (
-    [Parameter(Mandatory)]$BuildDir
-)
-Copy-Item -Path $BuildDir -Destination "C:\Program Files (x86)\Steam\steamapps\common\Pathfinder Second Adventure\Mods\TabletopTweaks - Development" -Recurse
-Copy-Item -Path $BuildDir -Destination "C:\Program Files\Unity Mod Manager\Pathfinder Second Adventure\TabletopTweaks -Development"-Recurse
