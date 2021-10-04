143344403574ebb28c92536aaf04475e02c7873d
Sean Petrie <sean.petrie17@gmail.com>
2021-10-04 14:51:10 -0500

    Removed the debug output

--- TabletopTweaks/MechanicsChanges/DRRework.cs
+++ TabletopTweaks/MechanicsChanges/DRRework.cs
@@ -330,7 +330,7 @@ namespace TabletopTweaks.MechanicsChanges {
         }
 
         [HarmonyPatch]
-        [HarmonyDebug]
+        //[HarmonyDebug]
         static class UIUtilityItem_Patches {
 
             static IEnumerable<MethodBase> TargetMethods() {
