af8d2736bbe7e88c4a4aba8138ae648c5ae01253
Bert Guns <bertguns@gmail.com>
2021-10-07 22:05:45 +0200

    Fix non-functional rage powers for primalist (#192)
    * Fix Celestial Totem line for Primalist
    
    * Fix Daemon Totem line powers not working during bloodrage
    
    * Fix Fiend Totem line powers not working during bloodrage
    
    * Fix Powerful Stance not working during Bloodrage. Fix Crippling/Wrecking Blows being active even while not raging. Add patched log statements.
    
    * Fix Scent rage power not working during Bloodrage

--- TabletopTweaks/Bugfixes/Classes/Barbarian.cs
+++ TabletopTweaks/Bugfixes/Classes/Barbarian.cs
@@ -1,5 +1,9 @@
 ﻿using HarmonyLib;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
 
 namespace TabletopTweaks.Bugfixes.Classes {
     class Barbarian {
@@ -13,8 +17,24 @@ namespace TabletopTweaks.Bugfixes.Classes {
                 Main.LogHeader("Patching Barbarian");
 
                 PatchBase();
+                PatchWreckingBlows();
+                PatchCripplingBlows();
             }
             static void PatchBase() {
+            }
+
+            static void PatchWreckingBlows() {
+                var WreckingBlowsFeature = Resources.GetBlueprint<BlueprintFeature>("5bccc86dd1f187a4a99f092dc054c755");
+                var PowerfulStanceEffectBuff = Resources.GetBlueprint<BlueprintBuff>("aabad91034e5c7943986fe3e83bfc78e");
+                WreckingBlowsFeature.GetComponent<BuffExtraEffects>().m_CheckedBuff = PowerfulStanceEffectBuff.ToReference<BlueprintBuffReference>();
+                Main.LogPatch("Patched", WreckingBlowsFeature);
+            }
+
+            static void PatchCripplingBlows() {
+                var CripplingBlowsFeature = Resources.GetBlueprint<BlueprintFeature>("0eec6efbb7f66e148817c9f51b804f08");
+                var PowerfulStanceEffectBuff = Resources.GetBlueprint<BlueprintBuff>("aabad91034e5c7943986fe3e83bfc78e");
+                CripplingBlowsFeature.GetComponent<BuffExtraEffects>().m_CheckedBuff = PowerfulStanceEffectBuff.ToReference<BlueprintBuffReference>();
+                Main.LogPatch("Patched", CripplingBlowsFeature);
             }
         }
     }
