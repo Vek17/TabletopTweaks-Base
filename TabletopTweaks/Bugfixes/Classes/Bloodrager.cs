adc23db0978819c5c9330ed93328ddb574e83d83
Bert Guns <bertguns@gmail.com>
2021-10-07 19:36:52 +0200

    Fix Powerful Stance not working during Bloodrage. Fix Crippling/Wrecking Blows being active even while not raging. Add patched log statements.

--- TabletopTweaks/Bugfixes/Classes/Bloodrager.cs
+++ TabletopTweaks/Bugfixes/Classes/Bloodrager.cs
@@ -185,6 +185,7 @@ namespace TabletopTweaks.Bugfixes.Classes {
                 PatchCelestialTotem();
                 PatchDaemonTotem();
                 PatchFiendTotem();
+                PatchPowerfulStance();
 
                 void PatchCelestialTotem() {
 
@@ -195,6 +196,7 @@ namespace TabletopTweaks.Bugfixes.Classes {
                         c.m_CheckedBuff = BloodragerStandartRageBuff.ToReference<BlueprintBuffReference>();
                         c.m_ExtraEffectBuff = CelestialTotemLesserBuff.ToReference<BlueprintBuffReference>();
                     });
+                    Main.LogPatch("Patched", CelestialTotemLesserFeature);
 
                     var CelestialTotemFeature = Resources.GetBlueprint<BlueprintFeature>("5156331dc888e9347ae6fc81ad3f3cec");
                     var CelestialTotemAreaBuff = Resources.GetBlueprint<BlueprintBuff>("7bf740b33eaa2534e91def3cef142e00");
@@ -203,6 +205,7 @@ namespace TabletopTweaks.Bugfixes.Classes {
                         c.m_CheckedBuff = BloodragerStandartRageBuff.ToReference<BlueprintBuffReference>();
                         c.m_ExtraEffectBuff = CelestialTotemAreaBuff.ToReference<BlueprintBuffReference>();
                     });
+                    Main.LogPatch("Patched", CelestialTotemFeature);
 
                     var CelestialTotemGreaterFeature = Resources.GetBlueprint<BlueprintFeature>("774f79845d1683a43aa42ebd2a549497");
                     var CelestialTotemGreaterBuff = Resources.GetBlueprint<BlueprintBuff>("e31276241f875254cb102329c0b55ba7");
@@ -213,6 +216,7 @@ namespace TabletopTweaks.Bugfixes.Classes {
                         c.m_CheckedBuff = BloodragerStandartRageBuff.ToReference<BlueprintBuffReference>();
                         c.m_ExtraEffectBuff = CelestialTotemGreaterBuff.ToReference<BlueprintBuffReference>();
                     });
+                    Main.LogPatch("Patched", CelestialTotemGreaterFeature);
                 }
 
                 void PatchDaemonTotem() {
@@ -223,6 +227,7 @@ namespace TabletopTweaks.Bugfixes.Classes {
                         c.m_CheckedBuff = BloodragerStandartRageBuff.ToReference<BlueprintBuffReference>();
                         c.m_ExtraEffectBuff = DaemonTotemLesserBaseBuff.ToReference<BlueprintBuffReference>();
                     });
+                    Main.LogPatch("Patched", DaemonTotemLesserFeature);
 
                     var DaemonTotemFeature = Resources.GetBlueprint<BlueprintFeature>("d673c30720e8e7c4bb0903dc3c9ab649");
                     var DaemonTotemBuff = Resources.GetBlueprint<BlueprintBuff>("a4195deeb13eb9c4b93b6987839b60c7");
@@ -231,6 +236,7 @@ namespace TabletopTweaks.Bugfixes.Classes {
                         c.m_CheckedBuff = BloodragerStandartRageBuff.ToReference<BlueprintBuffReference>();
                         c.m_ExtraEffectBuff = DaemonTotemBuff.ToReference<BlueprintBuffReference>();
                     });
+                    Main.LogPatch("Patched", DaemonTotemFeature);
 
                     var DaemonTotemGreaterFeature = Resources.GetBlueprint<BlueprintFeature>("9a2f0ffe517d221459640a4ad85710d7");
                     var DaemonTotemGreaterBuff = Resources.GetBlueprint<BlueprintBuff>("2cf21dce5ecc791449f3106fcd0b60c3");
@@ -239,6 +245,7 @@ namespace TabletopTweaks.Bugfixes.Classes {
                         c.m_CheckedBuff = BloodragerStandartRageBuff.ToReference<BlueprintBuffReference>();
                         c.m_ExtraEffectBuff = DaemonTotemGreaterBuff.ToReference<BlueprintBuffReference>();
                     });
+                    Main.LogPatch("Patched", DaemonTotemGreaterFeature);
                 }
 
                 void PatchFiendTotem() {
@@ -247,14 +254,28 @@ namespace TabletopTweaks.Bugfixes.Classes {
                     var FiendTotemLesserFeature = Resources.GetBlueprint<BlueprintFeature>("76437492f801f054ba536473ad2fde79");
                     var FiendTotemLesserRageBuff = Resources.GetBlueprint<BlueprintBuff>("d0649010d93907745a44034ad6eeeb5e");
                     PrimalistRagePowersBuff.AddConditionalBuff(FiendTotemLesserFeature, FiendTotemLesserRageBuff);
+                    Main.LogPatch("Patched", FiendTotemLesserFeature);
 
                     var FiendTotemFeature = Resources.GetBlueprint<BlueprintFeature>("ce449404eeb4a7c499fbe0248056174f");
                     var FiendTotemRageBuff = Resources.GetBlueprint<BlueprintBuff>("4f524a75bb13f7c40806b0c19dc06fe4");
                     PrimalistRagePowersBuff.AddConditionalBuff(FiendTotemFeature, FiendTotemRageBuff);
+                    Main.LogPatch("Patched", FiendTotemFeature);
 
                     var FiendTotemGreaterFeature = Resources.GetBlueprint<BlueprintFeature>("1105632657d94d940a43707a3a57b006");
                     var FiendTotemGreaterRageBuff = Resources.GetBlueprint<BlueprintBuff>("c84ca8f21f63c8249a192f34195f8787");
                     PrimalistRagePowersBuff.AddConditionalBuff(FiendTotemGreaterFeature, FiendTotemGreaterRageBuff);
+                    Main.LogPatch("Patched", FiendTotemGreaterFeature);
+                }
+
+                void PatchPowerfulStance() {
+                    var PowerfulStanceSwitchBuff = Resources.GetBlueprint<BlueprintBuff>("539e480bcfe6d6f48bdd90418240b50f");
+                    var PowerfulStanceEffectBuff = Resources.GetBlueprint<BlueprintBuff>("aabad91034e5c7943986fe3e83bfc78e");
+
+                    PowerfulStanceSwitchBuff.AddComponent<BuffExtraEffects>(c => {
+                        c.m_CheckedBuff = BloodragerStandartRageBuff.ToReference<BlueprintBuffReference>();
+                        c.m_ExtraEffectBuff = PowerfulStanceEffectBuff.ToReference<BlueprintBuffReference>();
+                    });
+                    Main.LogPatch("Patched", PowerfulStanceSwitchBuff);
                 }
             }
         }
