c8be72464c657c5f37a85f162d1a461351f67bc8
Sean Petrie <sean.petrie17@gmail.com>
2021-10-07 17:32:08 -0500

    Updated logic

--- TabletopTweaks/Bugfixes/General/BlueprintProgressionFix.cs
+++ TabletopTweaks/Bugfixes/General/BlueprintProgressionFix.cs
@@ -1,15 +1,13 @@
 ﻿using HarmonyLib;
 using JetBrains.Annotations;
-using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
 using Kingmaker.UnitLogic;
 using Kingmaker.Utility;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace TabletopTweaks.MechanicsChanges {
-    class ProgressionFix {
+    class BlueprintProgressionFix {
 
         [HarmonyPatch(typeof(BlueprintProgression), nameof(BlueprintProgression.CalcLevel))]
         static class BlueprintProgression_AllOtherClasses_CalcLevel_Fix {
@@ -17,11 +15,12 @@ namespace TabletopTweaks.MechanicsChanges {
             static void Postfix(BlueprintProgression __instance, ref int __result, [NotNull] UnitDescriptor unit) {
                 if (!__instance.ForAllOtherClasses) { return; }
                 Main.Log($"{__instance.name} - {__result}");
+                //Old Progression logic so we can remove it from the total
                 using (List<ClassData>.Enumerator enumerator = unit.Progression.Classes.GetEnumerator()) {
                     int originalIncrease = 0;
                     while (enumerator.MoveNext()) {
                         ClassData c = enumerator.Current;
-                        if (!__instance.m_Classes.HasItem((BlueprintProgression.ClassWithLevel i) => i.Class == c.CharacterClass) 
+                        if (!__instance.m_Classes.HasItem((BlueprintProgression.ClassWithLevel i) => i.Class == c.CharacterClass)
                             || !c.Archetypes.HasItem((BlueprintArchetype a) => __instance.m_Archetypes.HasItem((BlueprintProgression.ArchetypeWithLevel i) => i.Archetype == a))) {
                             originalIncrease += c.Level;
                         }
@@ -33,13 +32,17 @@ namespace TabletopTweaks.MechanicsChanges {
                     }
                     Main.Log($"Original Increase: {originalIncrease}");
                     __result -= originalIncrease;
-                    Main.Log($"TotalValue: {__result}");
+                    Main.Log($"TotalValue: {__result}\n");
                 }
+                //New correct logic
                 int newIncrease = 0;
                 foreach (ClassData characterClass in unit.Progression.Classes.Where(c => !c.CharacterClass.IsMythic)) {
-                    if (!__instance.m_Classes.HasItem((BlueprintProgression.ClassWithLevel i) => i.Class == characterClass.CharacterClass)
-                        && !characterClass.Archetypes.HasItem((BlueprintArchetype a) => __instance.m_Archetypes.HasItem((BlueprintProgression.ArchetypeWithLevel i) => i.Archetype == a))) {
-                        newIncrease += 1;
+                    bool InClasses = __instance.m_Classes.Any(classWithLevel => classWithLevel.Class == characterClass.CharacterClass);
+                    bool ClassHasArchetype = __instance.m_Archetypes.Any(archetypeWithLevel => characterClass.CharacterClass.Archetypes.HasReference(archetypeWithLevel.Archetype));
+                    bool NotValidArchetype = !__instance.m_Archetypes.Any(archetypeWithLevel => characterClass.Archetypes.Contains(archetypeWithLevel.Archetype));
+
+                    if (!InClasses || (InClasses && ClassHasArchetype && NotValidArchetype)) {
+                        newIncrease += characterClass.Level;
                     }
                 }
                 switch (__instance.AlternateProgressionType) {
@@ -47,9 +50,7 @@ namespace TabletopTweaks.MechanicsChanges {
                         newIncrease /= 2;
                         break;
                 }
-                Main.Log($"New Increase: {newIncrease}");
                 __result += newIncrease;
-                Main.Log($"TotalValue: {__result}");
             }
         }
     }
