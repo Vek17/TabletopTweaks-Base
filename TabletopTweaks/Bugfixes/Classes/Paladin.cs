27b1d68173c134ddccee5e55c80e8a367050f8dc
Sean Petrie <sean.petrie17@gmail.com>
2022-01-05 22:19:20 -0600

    More smites

--- TabletopTweaks/Bugfixes/Classes/Paladin.cs
+++ TabletopTweaks/Bugfixes/Classes/Paladin.cs
@@ -43,14 +43,23 @@ namespace TabletopTweaks.Bugfixes.Classes {
                     var SmiteChaosBuff = Resources.GetBlueprint<BlueprintBuff>("161051816b1530843a8096167be9b8a7");
                     var SmiteEvilBuff = Resources.GetBlueprint<BlueprintBuff>("b6570b8cbb32eaf4ca8255d0ec3310b0");
                     var AuraOfJusticeSmiteEvilBuff = Resources.GetBlueprint<BlueprintBuff>("ac3c66782859eb84692a8782320ffd2c");
+                    var CelestialSmiteEvilBuff = Resources.GetBlueprint<BlueprintBuff>("db4abdd3a772eec4c97048c1cf4b7417");
+                    var FiendishSmiteGoodBuff = Resources.GetBlueprint<BlueprintBuff>("a9035e49d6d79a64eaec321f2cb629a8");
+                    var HalfFiendSmiteGoodBuff = Resources.GetBlueprint<BlueprintBuff>("114af78efc58e5a4c86bb12ee1d907cc");
 
                     SmiteChaosBuff.GetComponent<AttackBonusAgainstTarget>().Descriptor = (ModifierDescriptor)Untyped.Charisma;
                     SmiteEvilBuff.GetComponent<AttackBonusAgainstTarget>().Descriptor = (ModifierDescriptor)Untyped.Charisma;
                     AuraOfJusticeSmiteEvilBuff.GetComponent<AttackBonusAgainstTarget>().Descriptor = (ModifierDescriptor)Untyped.Charisma;
+                    CelestialSmiteEvilBuff.GetComponent<AttackBonusAgainstTarget>().Descriptor = (ModifierDescriptor)Untyped.Charisma;
+                    FiendishSmiteGoodBuff.GetComponent<AttackBonusAgainstTarget>().Descriptor = (ModifierDescriptor)Untyped.Charisma;
+                    HalfFiendSmiteGoodBuff.GetComponent<AttackBonusAgainstTarget>().Descriptor = (ModifierDescriptor)Untyped.Charisma;
 
                     Main.LogPatch("Patched", SmiteChaosBuff);
                     Main.LogPatch("Patched", SmiteEvilBuff);
                     Main.LogPatch("Patched", AuraOfJusticeSmiteEvilBuff);
+                    Main.LogPatch("Patched", CelestialSmiteEvilBuff);
+                    Main.LogPatch("Patched", FiendishSmiteGoodBuff);
+                    Main.LogPatch("Patched", HalfFiendSmiteGoodBuff);
                 }
             }
 
